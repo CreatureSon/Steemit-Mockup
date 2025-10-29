5b0cae3b3c46760309088b8a9aefa2dad43399ae
CreatureSon <jacob.creech.na@gmail.com>
2025-10-29 14:31:40 -0500

    Added needed .gitignore files.  Updated html and views to operate with post data and users.  Next update will be adding votes, etc.

--- mockup/models.py
+++ mockup/models.py
@@ -1,24 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Post(models.Model):
-    post_name = models.CharField(max_length=200)
-    post_image = models.ImageField(upload_to='images/')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts", default=1)
+    title = models.CharField(max_length=75)
+    text = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='images/')
+    date = models.DateTimeField(auto_now_add=True)
+    votes = models.IntegerField(default=0)
+    commentCount = models.IntegerField(default=0)
+    tokens = models.FloatField(default=0.0)
+
+    def save(self, *args, **kwargs):
+        self.tokens = self.votes * 0.4
+        super().save(*args, **kwargs)
+
+    #//TODO
+    # Future method for tokens possibly
+    # from django.db.models import F
+
+    # tokens = models.GeneratedField(
+    #    expression=F('votes') * 0.4,
+    #    output_field=models.FloatField(),
+    #    db_persist=True,
+    # )
 
     def __str__(self):
-        return self.post_name
-
-""" class Topping(models.Model):
-    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
-    topping_name = models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.topping_name """
+        return self.title
     
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField()
-    user = models.CharField(max_length=100)
-    date_added = models.DateTimeField(auto_now_add=True)
+    date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.text[:50]}..."
--- mockup/views.py
+++ mockup/views.py
@@ -1,26 +1,40 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from .models import *
 from .forms import *
 
 def index(request):
     return render(request, 'mockup/index.html')
 
+@login_required
 def posts(request):
     posts = Post.objects.all()
 
-    context = {'posts': posts}
+    context = {
+        'posts': posts,
+        'icons': posts, #ICONS
+        'participant': posts #participant
+        }
 
     return render(request, "mockup/posts.html", context)
 
+@login_required
 def post(request, post_id):
     post = Post.objects.get(id=post_id)
 
     comments = Comment.objects.filter(post=post)
 
-    context = {'post': post, 'comments': comments}
+    context = {
+        'post': post, 
+        'comments': comments,
+        'icons': comments, #ICONS
+        'participant': post #participant
+        #'showCommentSeparator': comment.length
+        }
 
     return render(request, "mockup/post.html", context)
 
+@login_required
 def new_comment(request, post_id):
     post = Post.objects.get(id=post_id)
 
